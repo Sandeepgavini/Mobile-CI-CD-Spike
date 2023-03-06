ebf6549f513765cba7fd58d1d9b412614ad9cdff
Sai Sandeep <gavini.sai@visma.com>
2023-03-06 15:38:45 +0530

    Style changes

--- Screens/ProfilePic.js
+++ Screens/ProfilePic.js
@@ -6,7 +6,7 @@ const ProfilePic = () => {
     <View>
         <ImageBackground
             resizeMode='cover'
-            style = {{width:'100%',height:'auto'}}
+            style = {{width:'100%'}}
             spurce = {{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png',}}
         />
     </View>
